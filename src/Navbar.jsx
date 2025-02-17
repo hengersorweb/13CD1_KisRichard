ead57f0510e4603b0954e18e74dc80201b3f2603
Kis Richárd <kisric519@hengersor.hu>
2025-02-17 12:14:43 +0100

    menü elkészítése css kialakítása

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,10 +4,13 @@ import { Link, NavLink } from 'react-router-dom'
 const Navbar = (() => {
     return (
         <div>
-            <nav>
-                <Link to="/">Főoldal</Link>
-                <NavLink to="/ermesek">Az olimpia érmesei</NavLink>
-                <NavLink to="/mermesek">A magyar érmesek</NavLink>
+            <nav className="pa3 pa4-ns bb bt">
+                <Link className="link dim black b f1 f-headline-ns tc db mb1 mb1-ns" to="/">Covid</Link>
+                <div className="tc pb3">
+                    <NavLink className="link dim gray f6 f5-ns dib mr3" to="/">Főoldal</NavLink>
+                    <NavLink className="link dim gray f6 f5-ns dib mr3" to="/ermesek">Az olimpia érmesei</NavLink>
+                    <NavLink className="link dim gray f6 f5-ns dib mr3" to="/mermesek">A magyar érmesek</NavLink>
+                </div>
             </nav>
         </div>
     )
