099471a4ac0f14be16e289dbfec4bc999bf255ff
Kis Richárd <kisric519@hengersor.hu>
2025-02-17 11:29:12 +0100

    szükséges modulok import
    bootstrap, router, tachyons

---
+++ src/oldalak/Mermesek.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Mermesek = (() => {
+    return(
+        <div>
+            <h1>Az olimpiai magyar érmesei</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, voluptatum porro. Veritatis eum tenetur possimus id quis pariatur dolor quod laboriosam, adipisci repellat et quisquam totam excepturi esse sed fuga ex maxime distinctio reprehenderit sequi doloribus. Possimus cum sunt eius! Quia, enim accusamus facilis voluptatibus repellendus, ipsa tempore nihil voluptatem quisquam quae laborum commodi minima? Aliquam explicabo veritatis possimus optio facere aliquid velit laborum minima sit quas consequatur, tenetur quam, recusandae, voluptatem ex unde incidunt. Nisi dolorem, ab cum voluptate voluptates quibusdam fugiat nesciunt quos magni repellendus consequatur incidunt, vero optio velit id impedit porro sed rerum expedita, quas molestias.</p>
+        </div>
+    )
+})
+
+export default Mermesek
